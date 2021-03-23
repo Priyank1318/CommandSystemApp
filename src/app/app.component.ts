8cae4602f32b06f52da1c6c8d15c63f830432535
Chittineni, Priyanka <Priyanka.Chittineni@fmr.com>
2021-03-23 09:32:25 -0400

    commandApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Command } from './model/command';
+import { CommandService } from './services/command.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'CommandSystemsApplication';
+  cols: { field: string; header: string; }[] = [];
+  commands: Command[] = [];
+  commandStateSubscription: Subscription = new Subscription();
+  totalQuantity = 0;
+  averageRate = 0;
+  combinableAttacksList: string[] = [];
+
+  constructor(private commandService: CommandService) { }
+  ngOnDestroy(): void {
+    this.commandStateSubscription?.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.getCommands();
+    this.cols = [
+      { field: 'id', header: 'Command Id' },
+      { field: 'battleship', header: 'Battleship' },
+      { field: 'weaponSystem', header: 'Weapon System' },
+      { field: 'target', header: 'Target' },
+      { field: 'quantity', header: 'Quantity' },
+      { field: 'rate', header: 'Rate' }
+
+    ];
+
+  }
+
+
+  getCommands() {
+    this.commandService.getCommands().subscribe((data: Command[]) => {
+      console.log(data);
+      this.commands = data;
+      this.updateSummaryDetails();
+    });
+  }
+
+  updateSummaryDetails() {
+    this.totalQuantity = this.commands.map(a => a.quantity).reduce((a, b) => {
+      return a + b;
+    });
+    this.averageRate = (this.commands.map(a => a.rate).reduce((a, b) => {
+      return a + b;
+    })) / this.commands.length;
+    const attacks: string[] = [];
+    this.commands.forEach((command) => {
+      if ((this.commands.filter((el) => el.target === command.target
+        && el.battleship === command.battleship
+        && el.weaponSystem === command.weaponSystem).length) >= 2) {
+        const attack = command.battleship + '|' + command.target + '|' + command.weaponSystem;
+        if (attacks.indexOf(attack) == -1) {
+          attacks.push(attack);
+        }
+      }
+    });
+    this.combinableAttacksList = attacks;
+
+  }
+
+  getAttackDetails(attack: string): string {
+    const attackParts = attack.split('|');
+    const battleship = attackParts[0];
+    const target = attackParts[1];
+    const weaponSystem = attackParts[2];
+    const commandsList = this.commands.filter(command =>
+    command.battleship === battleship && command.target === target && command.weaponSystem === weaponSystem);
+    const quantity = commandsList.map(a => a.quantity).reduce((a, b) => {
+      return a + b;
+    });
+    const rate = (commandsList.map(a => a.rate).reduce((a, b) => {
+      return a + b;
+    })) / commandsList.length;
+    return `(${battleship},${weaponSystem},${target}), quantity=${quantity}, rate=${rate}`;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TableModule } from 'primeng/table';
+import { DialogService } from 'primeng/dynamicdialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
+import { AddCommandComponent } from './components/add-command/add-command.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCommandComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      DataService, { dataEncapsulation: false }
+    ),
+    TableModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService, DialogService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
